20bc8bc30f99ed68e1d91e5a06ee0dc5da9cdd0c
Tim Douglas <me@timdouglas.net>
2020-03-17 07:04:18 +0000

    added the extra growUp() features

--- src/pet.js
+++ src/pet.js
@@ -2,10 +2,18 @@ class Pet {
     constructor(name) {
         this.name = name || "An unloved pet";
         this.age = 0;
+        this.hunger = 0;
+        this.fitness = 10;
     }
 
     growUp() {
-        this.age = this.age + 1;
+        const AGING_FACTOR = 1;
+        const HUNGER_FACTOR = 5;
+        const FITNESS_FACTOR = 3;
+
+        this.age = this.age + AGING_FACTOR;
+        this.hunger = this.hunger + HUNGER_FACTOR;
+        this.fitness = this.fitness - FITNESS_FACTOR;
     }
 }
 
--- test/pet.test.js
+++ test/pet.test.js
@@ -5,14 +5,20 @@ describe('constructor', () => {
         expect(new Pet()).toBeInstanceOf(Object);
         expect(new Pet().name).toBe("An unloved pet");
         expect(new Pet('Manny').name).toBe("Manny");
-    }),
+    })
 
     it('should return a new pet with an age', () => {
         expect(new Pet().age).toBe(0);
     })
+
+    it('shold return a new pet with a hunger', () => {
+        expect(new Pet().hunger).toBe(0);
+    })
+
+    it('shold return a new pet with a fitness', () => {
+        expect(new Pet().fitness).toBe(10);
+    })
 })
-
-
 
 describe('growUp', () => {
     it('should make the pet grow', () => {
@@ -23,5 +29,24 @@ describe('growUp', () => {
         testPet.growUp();
         testPet.growUp();
         expect(testPet.age).toBe(4);
+    })
+
+    it('should make the pet hunger increase', () => {
+        const testPet = new Pet();
+        testPet.growUp();
+        expect(testPet.hunger).toBe(5);
+        testPet.growUp();
+        testPet.growUp();
+        testPet.growUp();
+        expect(testPet.hunger).toBe(20);
+    })
+
+    it('should make the pet fitness decrease', () => {
+        const testPet = new Pet();
+        testPet.growUp();
+        expect(testPet.fitness).toBe(7);
+        testPet.growUp();
+        testPet.growUp();
+        expect(testPet.fitness).toBe(1);
     })
 })

84b5f9a4195ebb308f654d2b002bc7ccc5422ac7
Tim Douglas <me@timdouglas.net>
2020-03-17 06:43:34 +0000

    grow up finished

--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,11 @@
 class Pet {
     constructor(name) {
         this.name = name || "An unloved pet";
+        this.age = 0;
+    }
+
+    growUp() {
+        this.age = this.age + 1;
     }
 }
 
--- test/pet.test.js
+++ test/pet.test.js
@@ -1,9 +1,17 @@
 const Pet = require('../src/pet');
 
 describe('pet', () => {
-    it('should return the pets name', () => {
+    it('should return a new pet with a name', () => {
         expect(new Pet()).toBeInstanceOf(Object);
         expect(new Pet().name).toBe("An unloved pet");
         expect(new Pet('Manny').name).toBe("Manny");
+    }),
+
+    it('should return a new pet with an age', () => {
+        const myPet = new Pet();
+        expect(myPet.age).toBe(0);
+        myPet.growUp();
+        myPet.growUp();
+        expect(myPet.age).toBe(2);
     })
 })
